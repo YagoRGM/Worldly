fccc21dab8f16cce19ad65666ec0abf2e9ffb9c7
Kaique <kaique1245br@gmail.com>
2025-05-27 10:23:30 -0300

    Supabase corrigido

--- src/Config/supabaseConfig.js
+++ src/Config/supabaseConfig.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ehnkoavwjcdciejqvcoe.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVobmtvYXZ3amNkY2llanF2Y29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNDI1OTksImV4cCI6MjA2MzkxODU5OX0.TMltv-3Dk7mWqYR7UECgEkREOdfvogqA96UwpAA39Ms';
+const SUPABASE_URL = 'https://rqlnquhnzozmnvstpbkx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxbG5xdWhuem96bW52c3RwYmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NjIzNjUsImV4cCI6MjA2MzMzODM2NX0.235tK40hLpHOs8HAJyywd5dPEKMJ5yLA-wCS5NMts80';
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     throw new Error('SupaBase URl ou chave não estão configuradas corretamente!');
